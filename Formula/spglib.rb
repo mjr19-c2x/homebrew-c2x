9ffc01f4fa343270bc335cbd5e7dce0efe608ddd
Michael Rutter <mjr19@c2x>
2023-02-06 16:59:03 +0000

    Add test for spglib

--- Formula/spglib.rb
+++ Formula/spglib.rb
@@ -5,6 +5,7 @@ class Spglib < Formula
   homepage "https://spglib.github.io/spglib/"
   url "https://github.com/spglib/spglib/archive/refs/tags/v2.0.2.zip"
   sha256 "3d08cb8bb882fc09cfbe8f0d2b67d3cc2ae1d45651dd75e9226c8e54bd0a09cf"
+  head "https://github.com/spglib/spglib/archive/master.zip"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
@@ -21,15 +22,13 @@ class Spglib < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test spglib`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    (testpath/"test.c").write <<~EOF
+      #include <spglib.h>
+      int main() {
+        return(spg_get_major_version()-2);
+      }
+    EOF
+    system ENV.cc, "-o", "a.out", "test.c", "-L#{lib}", "-lsymspg"
+    system "./a.out"
   end
 end
